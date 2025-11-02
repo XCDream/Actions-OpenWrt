154e649b7dfc14d82bbef58e47e57c86dde323a8
XCDream <76815376+XCDream@users.noreply.github.com>
2025-11-02 18:25:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,4 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/luci/bin/config_generate
